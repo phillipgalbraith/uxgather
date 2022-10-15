f3b59ef67f33f66cfc339e2a351da7d1749798cf
Phil G <fillupgalbraith@gmail.com>
2022-10-14 19:33:50 -0700

    added images and login

---
+++ pages/login.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+
+export default function Login() {
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form action="/send-data-here" method="post">
+  <label for="username">username</label>
+  <input
+    type="text"
+    id="username"
+    name="username"
+    required
+    minlength="10"
+    maxlength="20"
+  />
+  <label for="password">Password</label>
+  <input 
+    type="text" 
+    id="password" 
+    name="password" 
+    required 
+    minlength="8"
+    maxlength="20"
+  />
+  <button type="submit">Submit</button>
+</form>
+
+      <Link href="/">back to landing page</Link>
+    </div>
+  )
+}
